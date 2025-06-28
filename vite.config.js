2541718d71ceda775d69f5a39d4bb3fda3c1e990
Lakshya Singhal <157279723+lakshya-0264@users.noreply.github.com>
2025-06-28 17:49:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-
-  return {
+  const config = {
     plugins: [react()],
-    if (mode === 'development') {
-      server.proxy = {
+  };
+  if (mode === 'development') {
+    config.server = {
+      proxy: {
         '/api': {
           target: env.VITE_BACKEND_URL || 'http://127.0.0.1:8000',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
-      };
-    }
-  };
+      },
+    };
+  }
+
+  return config;
 });
