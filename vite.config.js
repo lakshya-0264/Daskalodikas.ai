a1d87a70d80cb2f1029ef6468b4984cbae94eba0
SAM8099 <samarthgarg8099@gmail.com>
2025-06-28 16:05:01 +0530

    env added

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '': {
-        target: 'https://daskalodikasai-fastapi-server.onrender.com',  // Your FastAPI backend URL
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://127.0.0.1:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-})
+  };
+});
