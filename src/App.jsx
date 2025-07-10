acc648127a330b2a8157364351cd9ddbbdba233a
Lakshya Singhal <157279723+lakshya-0264@users.noreply.github.com>
2025-07-11 00:12:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import ConversationPage from './components/ConversationPage';     // Updated pat
 
 function App() {
   return (
-   <>
-      <div className = "text-3xl font-semibold text-center text-red-500">Website is under maintenance.</div>
-      <div className = "text-xl text-center">Please wait for new features.</div>
-   </>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<ProblemSettingPage />} />
-    //     <Route path="/conversation" element={<ConversationPage />} />
-    //     {/* Optional: 404 page */}
-    //     <Route path="*" element={<div>Page Not Found</div>} />
-    //   </Routes>
-    // </Router>
+   // <>
+   //    <div className = "text-3xl font-semibold text-center text-red-500">Website is under maintenance.</div>
+   //    <div className = "text-xl text-center">Please wait for new features.</div>
+   // </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProblemSettingPage />} />
+        <Route path="/conversation" element={<ConversationPage />} />
+        {/* Optional: 404 page */}
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
